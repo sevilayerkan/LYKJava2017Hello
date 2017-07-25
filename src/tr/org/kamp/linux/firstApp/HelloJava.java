90efd2a8c9e192d1d3fede1cd5c3674f4b945dab
Sevilay Erkan <sevilayerkan2@gmail.com>
2017-07-25 19:03:11 +0300

    initial commit

---
+++ src/tr/org/kamp/linux/firstApp/HelloJava.java
@@ -0,0 +1,9 @@
+package tr.org.kamp.linux.firstApp;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		
+	}
+
+}
---
+++ src/tr/org/kamp/linux/veriTipleri/VeriTipleri.java
@@ -0,0 +1,10 @@
+package tr.org.kamp.linux.veriTipleri;
+
+public class VeriTipleri {
+	public int a = 65;
+	private int b = 45;
+	protected char = "A"
+	
+	int toplam = a + b;
+
+}
